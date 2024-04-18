c17e8ac7d2498d09c4d98b7c5a4eedbfbdeff7c2
Albert Jané Medràn <109897647+R3dH4wk44@users.noreply.github.com>
2024-04-18 21:09:57 +0000

    Añadiendo botones

--- about-us-astrodevs/src/components/Team/Team.jsx
+++ about-us-astrodevs/src/components/Team/Team.jsx
@@ -5,9 +5,9 @@ import  Maria  from '../../assets/maria.png';
 import  Cristina  from '../../assets/cristina.png';
 export const Team = () => {
     
-    const team = [{name:"María SF", image:Maria}, {name:"Andre Cortes", image:''},{name:"Cristina Benítez", image:Cristina},{name:"Alberto Limón", image:''}, {name:"Albert Jané", image:Albert} ];
+    const team = [{name:"María SF", image:Maria, rrss:['#','#']}, {name:"Andre Cortes", image:'', rrss:['#','#']},{name:"Cristina Benítez", image:Cristina, rrss:['#','#']},{name:"Alberto Limón", image:'', rrss:['#','#']}, {name:"Albert Jané", image:Albert, rrss:['https://www.linkedin.com/in/albert-jan%C3%A9-medr%C3%A0n-1016132b8/','https://github.com/R3dH4wk44']} ];
     const teamList = team.map(member => {
-        return (<Teammate name={member.name} image={member.image} />);
+        return (<Teammate name={member.name} image={member.image} rrss={member.rrss} />);
     })
     return (
         <>
@@ -15,9 +15,14 @@ export const Team = () => {
 
                 <h2 className='team__title'>Meet Our Team</h2>
                 
-                <p className='team__paragraph'>We are a team of Space passionates who are willing to take this project one step furder</p>
+                <p className='team__paragraph'>We are a team of space passionates who are willing to take this project one step furder</p>
                 <div className="cards__container">
                     {teamList}
+                </div>
+                <div>
+                    <button className='cards__contactus__button'>
+                        Contact Us!
+                    </button>
                 </div>
             </div>
         </>
