8331de4b4858b7186e1eecdaf13630e0e13bc521
Albert Jané Medràn <109897647+R3dH4wk44@users.noreply.github.com>
2024-04-18 22:41:50 +0000

    Añadiendo imagen de andre

--- about-us-astrodevs/src/components/Team/Team.jsx
+++ about-us-astrodevs/src/components/Team/Team.jsx
@@ -3,9 +3,10 @@ import { Teammate } from './Teammate';
 import  Albert  from '../../assets/albert.png';
 import  Maria  from '../../assets/maria.png';
 import  Cristina  from '../../assets/cristina.png';
+import Andre from '../../assets/andre.jpg';
 export const Team = () => {
     
-    const team = [{name:"María SF", image:Maria, rrss:['#','#']}, {name:"Andre Cortes", image:'', rrss:['#','#']},{name:"Cristina Benítez", image:Cristina, rrss:['#','#']},{name:"Alberto Limón", image:'', rrss:['#','#']}, {name:"Albert Jané", image:Albert, rrss:['https://www.linkedin.com/in/albert-jan%C3%A9-medr%C3%A0n-1016132b8/','https://github.com/R3dH4wk44']} ];
+    const team = [{name:"María SF", image:Maria, rrss:['#','https://github.com/DexpinBe']}, {name:"Andre Cortes", image:Andre, rrss:['#','https://github.com/andrecortes1997']},{name:"Cristina Benítez", image:Cristina, rrss:['#','https://github.com/Cristinacbm5']},{name:"Alberto Limón", image:'', rrss:['#','https://github.com/albertolimongithub']}, {name:"Albert Jané", image:Albert, rrss:['https://www.linkedin.com/in/albert-jan%C3%A9-medr%C3%A0n-1016132b8/','https://github.com/R3dH4wk44']} ];
     const teamList = team.map(member => {
         return (<Teammate name={member.name} image={member.image} rrss={member.rrss} />);
     })
@@ -19,10 +20,8 @@ export const Team = () => {
                 <div className="cards__container">
                     {teamList}
                 </div>
-                <div>
-                    <button className='cards__contactus__button'>
-                        Contact Us!
-                    </button>
+                <div className='team__container__button'>
+                    <a href="#" className='cards__contactus__button'>Contact Us!</a>                   
                 </div>
             </div>
         </>
