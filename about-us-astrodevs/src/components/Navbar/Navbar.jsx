a7d9bc7962aebc468a4c04da3d68195e68787de5
Cristinacbm5 <157827345+Cristinacbm5@users.noreply.github.com>
2024-04-19 06:56:38 +0000

    First Commit

--- about-us-astrodevs/src/components/Navbar/Navbar.jsx
+++ about-us-astrodevs/src/components/Navbar/Navbar.jsx
@@ -1,10 +1,26 @@
 import './Navbar.css';
+import Logo from './assets/Logo.JPG';
+import React from 'react';
 
 
 export const Navbar = () => {
     return (
-        <>
-        
-        </>
+        <nav className="navbar">
+            <div className="navbar__left">
+            <img src={Logo} alt="Logo" className="navbar__Logo" />
+                <h1 className="navbar__title days-one-regular navbar__title--larger">Astro Team</h1>
+            </div>
+            <div className="navbar__center">
+                <button className="navbar__button days-one-regular">About Project</button>
+                <button className="navbar__button days-one-regular">Team</button>
+                <button className="navbar__button days-one-regular">Form</button>
+                <button className="navbar__button days-one-regular">Footer</button>
+            </div>
+            <div className="navbar__right">
+                <button className="navbar__button days-one-regular">Contact Us</button>
+            </div>
+        </nav>
     );
 }
+
+export default Navbar;
