e66040bc26e21a637f090613c2fd791bb85da219
Andre Cortes <52327276+andrecortes1997@users.noreply.github.com>
2024-04-19 03:15:55 +0000

    Form Code Addittions

--- about-us-astrodevs/src/components/Form/Form.jsx
+++ about-us-astrodevs/src/components/Form/Form.jsx
@@ -1,9 +1,20 @@
 import './Form.css';
+import { Title } from './title/Title';
+import { Input } from './info_input/Input';
 
-export const Form = () => {
+
+
+
+ const Form = () => {
+
     return (
         <>
-        
+            <div className="form_Background" >
+                <Title />
+                <Input/>
+            </div>
         </>
     );
-}
+};
+
+export default Form
